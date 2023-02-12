f7f43c810afb8ce038e68bd8200fb3c897457ec2
Vadim Gredasov <v.gredasov@iva.ru>
2023-02-12 14:45:36 +0700

    Done

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -8,22 +8,33 @@ def index(request):
 
 
 def show_catalog(request):
-    template = 'catalog.html'
+    sort_dict = {
+        'name': 'name',
+        'min_price': 'price',
+        'max_price': '-price'
+    }
     phones = Phone.objects.all()
-    context = {}
-    for phone in phones:
-        context[phone.id] = {
-            'name': phone.name,
-            'image': phone.image,
-            'price': phone.price,
-            'release_date': phone.release_date,
-            'lte_exists': phone.lte_exists,
-            'slug': phone.slug}
-    print(context)
-    return render(request, template, context)
+    sort_key = request.GET.get('sort')
+
+    if sort_key:
+        phones = phones.order_by(sort_dict.get(sort_key, 'name'))
+    return render(
+        request,
+        'catalog.html',
+        context={'phones': phones}
+    )
 
 
 def show_product(request, slug):
+    phones_obj = Phone.objects.filter(slug=slug)
     template = 'product.html'
-    context = {}
+    for phone in phones_obj:
+        data = {
+            'name': phone.name,
+            'release_date': phone.release_date,
+            'price': phone.price,
+            'image': phone.image,
+            'lte_exists': phone.lte_exists
+        }
+    context = {'phone': data}
     return render(request, template, context)
