8270f652212afcd6477efc0726587396512413d1
Vadim Gredasov <v.gredasov@iva.ru>
2023-03-26 11:25:14 +0700

    Tests done

--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -26,11 +26,11 @@ def student_factory():
 @pytest.mark.django_db
 def test_get_first_course(client, course_factory):
     course_factory(_quantity=1)
-    response = client.get('/api/v1/courses/')
+    response = client.get('/api/v1/courses/1/')
     data = response.json()
     expected = Course.objects.all()
     assert response.status_code == 200
-    assert expected[0].id == data[0]['id']
+    assert expected[0].id == data['id']
 
 
 @pytest.mark.django_db
@@ -44,17 +44,17 @@ def test_check_courses_list(client, course_factory):
 @pytest.mark.django_db
 def test_check_filtered_data(client, course_factory):
     course_factory(_quantity=10)
-    response = client.get('/api/v1/courses/')
+    response = client.get('/api/v1/courses/', data={'id': 5})
     data = response.json()
     expected = Course.objects.get(id=5)
     assert response.status_code == 200
-    assert expected.id == data[4]['id']
+    assert expected.id == data[0]['id']
 
 
 @pytest.mark.django_db
 def test_check_name_filtered_data(client):
     Course(name='test').save()
-    response = client.get('/api/v1/courses/')
+    response = client.get('/api/v1/courses/', data={'id': 1})
     data = response.json()
     expected = Course.objects.get(name='test')
     assert response.status_code == 200
@@ -73,18 +73,18 @@ def test_create_course(client):
 @pytest.mark.django_db
 def test_update_course(client, course_factory):
     course_factory(_quantity=10)
-    response_put = client.put('/api/v1/courses/', data={'id': 2, 'name': 'test_create_course'})
-    response_get = client.get('/api/v1/courses/')
-    expected = Course.objects.get(id=2)
+    response_put = client.put('/api/v1/courses/1/', data={'name': 'test_create_course'})
+    response_get = client.get('/api/v1/courses/1/')
+    expected = Course.objects.get(name='test_create_course')
     data = response_get.json()
     assert response_put.status_code == 200
-    assert expected.name == data[0]['name']
+    assert expected.name == data['name']
 
 
 @pytest.mark.django_db
 def test_delete_course(client, course_factory):
     course_factory(_quantity=10)
-    response_delete = client.delete('/api/v1/courses/', data={'id': 2})
+    response_delete = client.delete('/api/v1/courses/1/')
     response_get = client.get('/api/v1/courses/')
-    assert response_delete.status_code == 200
+    assert response_delete.status_code == 204
     assert Course.objects.count() == len(response_get.json())
